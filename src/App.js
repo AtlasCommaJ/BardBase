c2e595e9ff655cbf497131b39b647f4eb860f249
AtlasCommaJ <atlascommaj@gmail.com>
2020-11-08 09:17:29 -0600

    add attribute to event

--- src/App.js
+++ src/App.js
@@ -47,7 +47,10 @@ const App = () => {
       setCurPlay(play);
       setCurScene("1.1");
       setCurRole([]);
-      Analytics.record({ name: play });
+      Analytics.record({
+        name: 'play', 
+        attributes: { title: play}
+      });
     },
     [setCurPlay, setCurScene, setCurRole]
   );
